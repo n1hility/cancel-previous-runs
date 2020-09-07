78266c1e138ad6adc712e5b9db4437039099993d
Ruben Werdmuller <46783746+RubenWerdmuller@users.noreply.github.com>
2020-09-07 09:17:44 +0200

    Fix deprecated package import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import * as github from '@actions/github'
 import * as core from '@actions/core'
-import Octokit from '@octokit/rest'
+import * as github from '@actions/github'
 import * as treemap from 'jstreemap'
+import { Octokit } from '@octokit/rest'
 
 function createRunsQuery(
   octokit: github.GitHub,
