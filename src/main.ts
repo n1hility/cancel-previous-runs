b9694e44156c7de61fa5d73360c5db8587be90aa
Jason T. Greene <jason.greene@redhat.com>
2023-01-31 21:30:18 -0600

    interim

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import * as github from '@actions/github'
+import { GitHub } from '@actions/github/lib/utils'
 import * as core from '@actions/core'
-import { Octokit } from '@octokit/rest'
+import * as otypes from '@octokit/types'
 import * as treemap from 'jstreemap'
 
 function createRunsQuery(
-  octokit: github.GitHub,
+  octokit: InstanceType<typeof GitHub>,
   owner: string,
   repo: string,
   workflowId: string,
   status: string,
   branch?: string,
   event?: string
-): Octokit.RequestOptions {
+): otypes.RequestOptions {
   const request =
     branch === undefined
       ? {
@@ -31,7 +32,7 @@ function createRunsQuery(
           event
         }
 
-  return octokit.actions.listWorkflowRuns.endpoint.merge(request)
+  return octokit.rest.actions.listWorkflowRuns.endpoint.merge(request)
 }
 
 async function cancelDuplicates(
@@ -43,12 +44,12 @@ async function cancelDuplicates(
   branch?: string,
   event?: string
 ): Promise<void> {
-  const octokit = new github.GitHub(token)
+  const octokit = github.getOctokit(token)
 
   // Determine the workflow to reduce the result set, or reference another workflow
   let resolvedId = ''
   if (workflowId === undefined) {
-    const reply = await octokit.actions.getWorkflowRun({
+    const reply = await octokit.rest.actions.getWorkflowRun({
       owner,
       repo,
       // eslint-disable-next-line @typescript-eslint/camelcase
@@ -82,7 +83,7 @@ async function cancelDuplicates(
       // different endpoint URL which trips up the resulting representation
       // In that case, fallback to the actual REST 'workflow_runs' property
       const elements =
-        item.data.length === undefined ? item.data.workflow_runs : item.data
+        item.data.length === undefined ? (item.data as any).workflow_runs : item.data
 
       for (const element of elements) {
         sorted.set(element.run_number, element)
@@ -184,7 +185,7 @@ async function run(): Promise<void> {
       branch,
       eventName
     )
-  } catch (error) {
+  } catch (error:any) {
     core.setFailed(error.message)
   }
 }
@@ -205,7 +206,7 @@ async function cancelRun(
       run_id: id
     })
     core.info(`Previous run (id ${id}) cancelled, status = ${reply.status}`)
-  } catch (error) {
+  } catch (error:any) {
     core.info(
       `[warn] Could not cancel run (id ${id}): [${error.status}] ${error.message}`
     )
